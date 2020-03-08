da277896d919ba2660799e7a5fc8493dd0f45c3d
genaromartinez <genaro.mtz1998@gmail.com>
2020-03-08 13:46:53 -0600

    Add new mission1

--- M1/Mission-1.c
+++ M1/Mission-1.c
@@ -1,199 +1,574 @@
 /*
-Author: Genaro Martínez S.
-id: A01566055
-date: 01/03/2020
-
-DOCUMENT 01: creating a new document registry system.
-*/
-
+ * Genaro Martínez S.
+ * A01566055
+ * Este programa usa un menu para registrar, buscar. editar y eliminar agentes, validando su informacion.
+ * 08/03/2019
+ */
 #include <stdio.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <ctype.h>
 #include <stdbool.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
-// crea la estructura del documento.
+// struct agent
 typedef struct
 {
-    char *name;
-    char *lName;
-    int age;
-    char *(activeId);
-    char *(missionId);
-} DOCUMENT;
+    char cNombre[10];
+    char cApellido[10];
+    int iNoAsignaciones;
+    char cAsignaciones[10][14];
+    int iEdad;
+    int iNoMisiones;
+    char cMision[10][13];
+} agent;
 
-void remove_element(DOCUMENT *array, int index, int array_length)
+//Funcion access
+//Esta funcion regresa un booleano que determinara si la información se muestra de manera cifrada o no.
+//cArreglo: Arreglo de apuntadores de caracteres que se modificaran en caso de ingresar una contraseña erronea.
+bool access(char *cArreglo[5])
 {
-   int i;
-   for(i = index; i < array_length - 1; i++) array[i] = array[i + 1];
-}
+    int iContraseña = 0;
+    int iEncriptar = 0;
+    char *ptr;
 
-// Revisa que el id de la misión funcione correctamente
-bool checkMission(char missionId[])
-{
-    int intErrors = 0, charErrors = 0, stringSize = 0;
+    printf("Ingresa la contraseña de acceso: ****\n");
+    scanf("%d", &iContraseña);
 
-    for (int i = 0; missionId[i] != '\0'; i++)
+    if (iContraseña == 1234)
     {
-        // revisa el tipo de dato.
-        if (i < 4 && isalpha(missionId[i]) == 0)
-        {
-            charErrors++;
-        }
-        if (i > 3 && isalnum(missionId[i]) == 0)
-        {
-            intErrors++;
-        }
-        stringSize++;
-    }
-
-    if (stringSize > 12)
-    {
-        printf("the mission id is longer than the maximum limit.\n");
-    }
-    else if (charErrors > 0)
-    {
-        printf("the mission id has integer errors.\n");
-        if (intErrors > 0)
-        {
-            printf("the mission id has character errors.\n");
-        }
+        printf("Bienvenido agente\n\n\n");
+        return true;
     }
     else
     {
-        printf("\n");
-        printf("mission data correctly created!");
-        return true;
+        printf("Contraseña incorrecta\n");
+        iEncriptar = 3;
+
+        for (int i = 0; i < 5; i++)
+        {
+            ptr = cArreglo[i];
+            while (*ptr != '\0')
+            {
+                *ptr = *ptr + iEncriptar;
+                *ptr++;
+            }
+        }
+        printf("%s\n\n%s\n%s\n%s\n%s\n\n", cArreglo[0], cArreglo[1], cArreglo[2], cArreglo[3], cArreglo[4]);
+        return false;
     }
-    return false;
 }
 
-// revisa cada caracter de un active id creado
-bool checkActive(char activeId[])
+//Funcion validateAsignee
+//Esta funcion valida que el string contenga 4 letras y 9 digitos.
+//cValidar: String a validar.
+bool validateAsignee(char cValidar[14])
 {
-    int intErrors = 0, charErrors = 0, stringSize = 0;
 
-    for (int i = 0; activeId[i] != '\0'; i++)
+    int letras = 0, digitos = 0, i;
+    for (i = 0; cValidar[i] != '\0'; i++)
     {
-        if (i < 5 && isalpha(activeId[i]) == 0)
-        {
-            charErrors++;
-        }
-        if (i > 4 && isalnum(activeId[i]) == 0)
-        {
-            intErrors++;
-        }
-        stringSize++;
+        // revisa letras
+        if (isalpha(cValidar[i]) != 0)
+            letras++;
+
+        // revisa digitos
+        else if (isdigit(cValidar[i]) != 0)
+            digitos++;
     }
 
-    if (stringSize > 13)
+    if (letras == 4 && digitos == 9)
     {
-        printf("the active id is longer than the maximum limit.\n");
-    }
-    else if (charErrors > 0)
-    {
-        printf("the active id has integer errors.\n");
-        if (intErrors > 0)
-        {
-            printf("the active id has character errors.\n");
-        }
+        return true;
     }
     else
     {
-        printf("\n");
-        printf("active data correctly created!");
-        return true;
+        return false;
     }
-    return false;
 }
 
-int main(int argc, char const *argv[])
+//Funcion validateMission
+//Esta funcion valida que el string contenga 3 letras y 9 digitos.
+//cValidar: String a validar.
+bool validateMission(char cValidar[14])
 {
-    char name[20];
-    char lName[20];
-    char missionId[20];
-    char activeId[20];
-    char *activesArr[20];
-    int entry, age, activesNo = 0, intErrors = 0, charErrors = 0, stringSize = 0;
-    char gender;
-    bool allCheck, missionCheck, activeCheck;
-    DOCUMENT mission;
-    DOCUMENT* missions = malloc(5 * sizeof *missions);
 
-printf("________________________\n");
-printf("    `'-.,________,,..-  ___ \________      \n");
-printf("       > `'-.,_____,,.-`<_>\ )__     `\    \n");
-printf("         >            `'-._    _)____  \   \n");
-printf("          >          _.-'`  _______  `\ |  \n");
-printf("           >   _,,..--'''```       `'-.\|  \n");
-printf("          >-'``                        `'    \n");
-printf("       ----ADVANCED PROGRAMMING SEMINARY----     ");
-printf("\n");
-
-printf("Options (1 or 2): \n");
-printf("1 create entry \n");
-printf("2 delete entry \n");
-scanf("%d", &entry);
-    if (entry == 1)
+    int letras = 0, digitos = 0, i;
+    for (i = 0; cValidar[i] != '\0'; i++)
     {
-        printf("name: ");
-        scanf("%s", &name);
-        printf("last name: ");
-        scanf("%s", &lName);
-        printf("age (integer): ");
-        scanf("%d", &age);
-        printf("enter number of mission actives: ");
-        scanf(" %d", &activesNo);
-        for (int i = 0; i < activesNo; i++)
-        {
-            printf("enter active %d: ", i + 1);
-            scanf("%s", activeId);
-            if (checkActive(activeId))
-            {
-                activesArr[i] = activeId;
-                /* code */
-            }
-            else
-            {
-                printf("unable to create the active with the requested data!\n");
-            }
-        }
+        // revisa letras
+        if (isalpha(cValidar[i]) != 0)
+            letras++;
 
-        printf("enter the mission id: ");
-        scanf(" %s", &missionId);
-        if (checkMission(missionId))
-        {
-            missionCheck = true;
-        }
+        // revisa digitos
+        else if (isdigit(cValidar[i]) != 0)
+            digitos++;
+    }
 
-        if (missionCheck && activeCheck)
+    if (letras == 3 && digitos == 9)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//Funcion entrada
+//Esta funcion asigna datos a un agente.
+//Agentes: Arreglo de agentes.
+//*iContador: Apuntador al numero de agentes registrados.
+void entrada(agent Agentes[10], int *iContador)
+{
+
+    char cValidar[14];
+
+    //Registrar nombre
+    printf("Teclea el nombre del agente:\n");
+    scanf("%s", Agentes[*iContador].cNombre);
+
+    //Registrar apellido
+    printf("Teclea tu apellido:\n");
+    scanf("%s", Agentes[*iContador].cApellido);
+
+    //Registrar numero de activos de inteligencia
+    printf("Teclea el numero de activos de inteligencia asignados: (maximo 10)\n");
+    scanf("%d", &Agentes[*iContador].iNoAsignaciones);
+
+    //Registrar activos de inteligencia
+    for (int i = 0; i < Agentes[*iContador].iNoAsignaciones; i++)
+    {
+        printf("Ingresa el agente numero %d\n", i + 1);
+        scanf("%s", cValidar);
+
+        //Validar Activo de inteligencia
+        if (validateAsignee(cValidar))
         {
-            mission = (DOCUMENT){name, lName, age, gender, missionId};
-            printf("\n");
-            printf("mission data correctly created!");
-            missions[0] = mission;
-            return 0;
+            strcpy(Agentes[*iContador].cAsignaciones[i], cValidar);
         }
         else
         {
-            printf("\n");
-            printf("unable to create the document with the requested data!\n");
+            printf("Codigo incompatible\n");
+            i--;
+        }
+    }
+
+    //Registrar edad
+    printf("Teclea tu edad:\n");
+    scanf("%d", &Agentes[*iContador].iEdad);
+
+    //Registrar numero de misiones
+    printf("Teclea el numero de misiones asignadas: (maximo 10)\n");
+    scanf("%d", &Agentes[*iContador].iNoMisiones);
+
+    //Registrar misiones
+    for (int i = 0; i < Agentes[*iContador].iNoMisiones; i++)
+    {
+        printf("Ingresa la mision numero %d\n", i + 1);
+        scanf("%s", cValidar);
+
+        //Validar Mision
+        if (validateMission(cValidar))
+        {
+            strcpy(Agentes[*iContador].cMision[i], cValidar);
+        }
+        else
+        {
+            printf("El codigo de la mision es incompatible. Reingresa el codigo\n");
+            i--;
+        }
+    }
+
+    printf("Datos capturados\n");
+
+    *iContador = *iContador + 1;
+}
+
+//Funcion eliminateEntrada
+//Esta funcion borra los datos de un agente.
+//Agentes: Arreglo de agentes.
+//*iContador: Apuntador al numero de agentes registrados.
+void eliminateEntrada(agent Agentes[10], int *iContador)
+{
+
+    printf("Entradas existentes: %d\n", *iContador);
+
+    if (*iContador != 0)
+    {
+
+        int iOpcion;
+
+        //Desplegar informacion
+        printf("Selecciona el indice que quieres eliminar\n");
+        for (int i = 0; i < *iContador; i++)
+        {
+            printf("%d: %s %s\n", i + 1, Agentes[i].cNombre, Agentes[i].cApellido);
+        }
+        printf("%d: Volver al menu\n", *iContador + 1);
+
+        scanf("%d", &iOpcion);
+
+        //Opcion para salir
+        if (iOpcion == *iContador + 1)
+        {
+            return;
+        }
+        else
+        {
+            for (int i = iOpcion; i < *iContador; i++)
+            {
+                Agentes[i - 1] = Agentes[i];
+            }
+            *iContador = *iContador - 1;
+            printf("Agente eliminado exitosamente\n");
         }
     }
     else
     {
-        char *deleteDocId[10];
-        printf("choose name of mission document to delete");
-        scanf("%s", &deleteDocId);
-        /* missing code to delete an agent registry */
-        for (int i = 0; i < sizeof *missions; i++)
+        printf("\n\nNo puedes eliminar ninguna entrada\n\n");
+    }
+}
+
+//Funcion editAgent
+//Esta funcion edita los activos y las misiones asignadas a un agente.
+//Agentes: Arreglo de agentes.
+//indice: Indice del agente a editar.
+void editAgent(agent Agentes[10], int indice)
+{
+
+    char cValidar[14];
+
+    //Registrar numero de activos de inteligencia
+    printf("Teclea el numero de activos de inteligencia asignados: (maximo 10)\n");
+    scanf("%d", &Agentes[indice].iNoAsignaciones);
+
+    //Registrar activos de inteligencia
+    for (int i = 0; i < Agentes[indice].iNoAsignaciones; i++)
+    {
+        printf("Ingresa el agente numero %d\n", i + 1);
+        scanf("%s", cValidar);
+
+        //Validar Activo de inteligencia
+        if (validateAsignee(cValidar))
         {
-            if(missions[i].name == deleteDocId) {
-                remove_element(missions, i, sizeof *missions);
-                printf("element successfully removed");
+            strcpy(Agentes[indice].cAsignaciones[i], cValidar);
+        }
+        else
+        {
+            printf("Codigo incompatible\n");
+            i--;
+        }
+    }
+
+    //Registrar numero de misiones
+    printf("Teclea el numero de misiones asignadas: (maximo 10)\n");
+    scanf("%d", &Agentes[indice].iNoMisiones);
+
+    //Registrar misiones
+    for (int i = 0; i < Agentes[indice].iNoMisiones; i++)
+    {
+        printf("Ingresa la mision numero %d\n", i + 1);
+        scanf("%s", cValidar);
+
+        //Validar Mision
+        if (validateMission(cValidar))
+        {
+            strcpy(Agentes[indice].cMision[i], cValidar);
+        }
+        else
+        {
+            printf("El codigo de la mision es incompatible. Reingresa el codigo\n");
+            i--;
+        }
+    }
+}
+
+//Funcion eliminateAgent
+//Esta funcion elimina a un agente.
+//Agentes: Arreglo de agentes.
+//*iContador: Apuntador al numero de agentes registrados.
+//indice: Indice del agente a editar.
+void eliminateAgent(agent Agentes[10], int *iContador, int indice)
+{
+
+    for (int i = indice + 1; i < *iContador; i++)
+    {
+        Agentes[i - 1] = Agentes[i];
+    }
+    *iContador = *iContador - 1;
+    printf("Agente eliminado exitosamente\n");
+}
+
+//Funcion findLName
+//Esta funcion busca coincidencias con apellido.
+//Agentes: Arreglo de agentes.
+//*iContador: Apuntador al numero de agentes registrados.
+void findLName(agent Agentes[10], int *iContador)
+{
+
+    char apellido[14];
+    int iIndices[10];
+    int iIndex = 0;
+    bool boton;
+    char *ptr;
+
+    printf("¿Que apellido buscas?\n");
+    scanf("%s", apellido);
+
+    //Busqueda
+    for (int i = 0; i < *iContador; i++)
+    {
+        boton = true;
+        ptr = Agentes[i].cApellido;
+        for (int j = 0; j < strlen(apellido); j++)
+        {
+            if (*ptr != apellido[j])
+            {
+                boton = false;
+                break;
+            }
+            *ptr++;
+        }
+        if (boton)
+        {
+            iIndices[iIndex] = i;
+            iIndex++;
+        }
+    }
+
+    printf("Entradas encontradas: %d\n", iIndex);
+
+    if (iIndex != 0)
+    {
+
+        int iOpcion;
+
+        //Desplegar informacion
+        printf("Selecciona un indice\n");
+        for (int i = 0; i < iIndex; i++)
+        {
+            printf("%d: %s %s\n", i + 1, Agentes[iIndices[i]].cNombre, Agentes[iIndices[i]].cApellido);
+        }
+        printf("%d: Volver al menu\n", iIndex + 1);
+
+        scanf("%d", &iOpcion);
+
+        //Opcion para salir
+        if (iOpcion == iIndex + 1)
+        {
+            return;
+        }
+        else
+        {
+
+            int iOpcion1;
+            printf("¿Que quieres hacer con el agente?\n1: Editar\n2: eliminateEntrada\n");
+            scanf("%d", &iOpcion1);
+
+            switch (iOpcion1)
+            {
+            case 1: //Editar
+                editAgent(Agentes, iIndices[iOpcion - 1]);
+                break;
+            case 2: //eliminateEntrada
+                eliminateAgent(Agentes, iContador, iIndices[iOpcion - 1]);
+                break;
+            default:
+                break;
             }
         }
-        
+    }
+    else
+    {
+        printf("\n\nNo se encontraron coincidencias\n\n");
+    }
+}
+
+//Funcion findActivo
+//Esta funcion busca coincidencias con los activos.
+//Agentes: Arreglo de agentes.
+//*iContador: Apuntador al numero de agentes registrados.
+void findActivo(agent Agentes[10], int *iContador)
+{
+
+    char activo[14];
+    int iIndices[10];
+    int iIndex = 0;
+    bool boton;
+    int ptr;
+    char *ptr1;
+
+    printf("¿Que activos de inteligencia buscas?\n");
+    scanf("%s", activo);
+
+    //Busqueda
+    for (int i = 0; i < *iContador; i++)
+    {
+        //boton = true;
+        ptr = Agentes[i].iNoAsignaciones;
+        for (int j = 0; j < ptr; j++)
+        {
+            boton = true;
+            ptr1 = Agentes[i].cAsignaciones[j];
+            for (int k = 0; k < strlen(activo); k++)
+            {
+                if (*ptr1 != activo[k])
+                {
+                    boton = false;
+                    break;
+                }
+                *ptr1++;
+            }
+            if (boton)
+            {
+                iIndices[iIndex] = i;
+                iIndex++;
+                break;
+            }
+        }
+    }
+
+    printf("Entradas encontradas: %d\n", iIndex);
+
+    if (iIndex != 0)
+    {
+
+        int iOpcion;
+
+        //Desplegar informacion
+        printf("Selecciona un indice\n");
+        for (int i = 0; i < iIndex; i++)
+        {
+            printf("%d: %s %s\n", i + 1, Agentes[iIndices[i]].cNombre, Agentes[iIndices[i]].cApellido);
+        }
+        printf("%d: Volver al menu\n", iIndex + 1);
+
+        scanf("%d", &iOpcion);
+
+        //Opcion para salir
+        if (iOpcion == iIndex + 1)
+        {
+            return;
+        }
+        else
+        {
+            /*for(int i = iOpcion;i<*iContador;i++){
+                Agentes[i-1] = Agentes[i];
+            }
+            *iContador = *iContador-1;
+            printf("Agente eliminado exitosamente\n");*/
+            int iOpcion1;
+
+            printf("¿Que quieres hacer con el agente?\n1: Editar\n2: eliminateEntrada\n");
+
+            scanf("%d", &iOpcion1);
+
+            switch (iOpcion1)
+            {
+            case 1: //Editar
+                editAgent(Agentes, iIndices[iOpcion - 1]);
+                break;
+            case 2: //eliminateEntrada
+                eliminateAgent(Agentes, iContador, iIndices[iOpcion - 1]);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    else
+    {
+        printf("\n\nNo se encontraron coincidencias\n\n");
+    }
+}
+
+//Funcion buscar
+//Esta funcion discierne entre buscar por apellido o por activo.
+//Agentes: Arreglo de agentes.
+//*iContador: Apuntador al numero de agentes registrados.
+void buscar(agent Agentes[10], int *iContador)
+{
+
+    int iOpcion;
+
+    printf("Por favor seleccione\n\n1: buscar por apellido\n2: buscar por activos\n");
+
+    scanf("%d", &iOpcion);
+
+    switch (iOpcion)
+    {
+    case 1: //buscar por apellido
+        findLName(Agentes, iContador);
+        break;
+    case 2: //buscar por activo
+        findActivo(Agentes, iContador);
+        break;
+    default:
+        break;
+    }
+}
+
+//Funcion principal
+int main()
+{
+
+    bool bContinuar = true;
+    int iOpcion = 2;
+    agent Agentes[10];
+    int iContador = 0;
+    char cSaludo[] = "Por favor seleccione:";
+    char cOpcion1[] = "1: Crear una nueva entrada";
+    char cOpcion2[] = "2: buscar una entrada";
+    char cOpcion3[] = "3: eliminateEntrada una entrada";
+    char cOpcion4[] = "4: Salir";
+    char *cArreglo[5];
+
+    cArreglo[0] = cSaludo;
+    cArreglo[1] = cOpcion1;
+    cArreglo[2] = cOpcion2;
+    cArreglo[3] = cOpcion3;
+    cArreglo[4] = cOpcion4;
+
+    //Cifrar datos
+    if (!access(cArreglo))
+    {
+        return 0;
+    }
+
+    while (bContinuar)
+    {
+        printf("____________________________\n");
+        printf("    `'-.,________,,..-  ___ \\_________      \n");
+        printf("       > `'-.,_____,,.-`<_>\\ )__     `\\    \n");
+        printf("         >            `'-._    _)____  \\   \n");
+        printf("          >          _.-'`  _______  `\\ |  \n");
+        printf("           >   _,,..--'''```       `'-.\\|  \n");
+        printf("          >-'``                        `'    \n");
+        printf("       ****ADVANCED PROGRAMMING SEMINARY****     ");
+        printf("\n");
+
+        printf("%s\n\n%s\n%s\n%s\n%s\n\n", cSaludo, cOpcion1, cOpcion2, cOpcion3, cOpcion4);
+
+        scanf("%d", &iOpcion);
+
+        switch (iOpcion)
+        {
+        case 1: //Registrar nuevo agente
+            entrada(Agentes, &iContador);
+            break;
+        case 2: //buscar agente agente
+            buscar(Agentes, &iContador);
+            break;
+        case 3: //eliminateEntrada un agente
+            eliminateEntrada(Agentes, &iContador);
+            break;
+        case 4: //Salir
+            printf("\nDatos almacenados!\nGraciar por usar el sistema.");
+            bContinuar = false;
+            break;
+        default:
+            break;
+        }
     }
     return 0;
 }
